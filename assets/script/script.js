b566e19d553ffadbcb5751603ab5ff3dd67938c3
AB <35473718+arsalanbardsiri@users.noreply.github.com>
2023-07-31 17:02:12 -0700

    Setting up times, proper time format, info background for btn.

--- assets/script/script.js
+++ assets/script/script.js
@@ -2,13 +2,25 @@
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
 $(function () {
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
+  function renderTimeBlocks() {
+    var businessHours = 9; // Start at 9am
+    var totalHours = 9; // 9am to 5pm
+  
+    for (let i = 0; i < totalHours; i++) {
+      var hour = businessHours + i;
+      var formattedHour = dayjs().hour(hour).format('h A');
+      var timeBlock = `
+        <div id="hour-${hour}" class="row time-block">
+          <div class="col-2 col-md-1 hour text-center py-3">${formattedHour}</div>
+          <textarea class="col-8 col-md-10 description" rows="3"></textarea>
+          <button type ="button" class="btn saveBtn col-2 col-md-1 bg-info" aria-label="save">
+            <i class="fas fa-save" aria-hidden="true"></i>
+          </button>
+        </div>
+      `;
+      $(".container-fluid").append(timeBlock);
+    }
+  }
   // TODO: Add code to apply the past, present, or future class to each time
   // block by comparing the id to the current hour. HINTS: How can the id
   // attribute of each time-block be used to conditionally add or remove the
@@ -24,6 +36,8 @@ $(function () {
   function displayCurrentDay() {
     $("#currentDay").text(dayjs().format("dddd, MMMM D, YYYY"));
   }
+
+  renderTimeBlocks();
   displayCurrentDay();
 
   // Update time block colors every minute to handle changes in time
